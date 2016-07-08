3f754f42e74ea36669d5ce296c2b69242c7bb1a6
Code Ass <aycabta@gmail.com>
2016-07-08 19:58:36 +0900

    Create hash table from apparent dots' binary to a braille character

--- list_braille.rb
+++ list_braille.rb
@@ -1,6 +1,68 @@
+num = 0
+num_to_braille = {}
 (0..0xF).each do |upper|
   (0..0xF).each do |downer|
-    print "#{[(0x2800 + upper * 0x10 + downer)].pack('U*')} "
+    bit_mapping =
+      ((num & 0b00000001) << 7) |
+      ((num & 0b00000010) << 5) |
+      ((num & 0b00000100) << 3) |
+      ((num & 0b00001000) << 1) |
+      ((num & 0b00010000) >> 1) |
+      ((num & 0b00100000) >> 3) |
+      ((num & 0b01000000) >> 5) |
+      ((num & 0b10000000) >> 7) |
+      0
+    num_to_braille[bit_mapping] = [(0x2800 + upper * 0x10 + downer)].pack('U*')
+    num += 1
   end
-  puts
 end
+
+puts "|#{num_to_braille[
+('10'+
+ '00'+
+ '00'+
+ '00').to_i(2)
+]}|"
+puts "|#{num_to_braille[
+('01'+
+ '00'+
+ '00'+
+ '00').to_i(2)
+]}|"
+puts "|#{num_to_braille[
+('00'+
+ '10'+
+ '00'+
+ '00').to_i(2)
+]}|"
+puts "|#{num_to_braille[
+('00'+
+ '01'+
+ '00'+
+ '00').to_i(2)
+]}|"
+puts "|#{num_to_braille[
+('00'+
+ '00'+
+ '10'+
+ '00').to_i(2)
+]}|"
+puts "|#{num_to_braille[
+('00'+
+ '00'+
+ '01'+
+ '00').to_i(2)
+]}|"
+puts "|#{num_to_braille[
+('00'+
+ '00'+
+ '00'+
+ '10').to_i(2)
+]}|"
+puts "|#{num_to_braille[
+('00'+
+ '00'+
+ '00'+
+ '01').to_i(2)
+]}|"
+
