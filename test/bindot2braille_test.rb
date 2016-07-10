2be53c61e96f7f437f5722f3cd0453ccf90b8475
Code Ass <aycabta@gmail.com>
2016-07-10 10:03:29 +0900

    Implement simple test

--- test/bindot2braille_test.rb
+++ test/bindot2braille_test.rb
@@ -1,11 +1,37 @@
 require 'test_helper'
+require 'bindot2braille'
+require 'bindot2braille/version'
 
-class BinDot2BrailleTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Bindot2braille::VERSION
+describe BinDot2Braille do
+  describe "version number" do
+    it "must be had" do
+      refute_nil(::BinDot2Braille::VERSION)
+    end
   end
 
-  def test_it_does_something_useful
-    assert false
+  describe "when asked about blending possibilities" do
+    it "won't say no" do
+      assert_equal(
+        BinDot2Braille.convert(
+          2, 4,
+          '01'+
+          '00'+
+          '10'+
+          '11'),
+        [['⣌']])
+      assert_equal(
+        BinDot2Braille.convert(
+          4, 8,
+          '0000'+
+          '0100'+
+          '1010'+
+          '1010'+
+          '1110'+
+          '1010'+
+          '1010'+
+          '0000'),
+        [['⡔', '⡄'],
+         ['⠏', '⠇']])
+    end
   end
 end
